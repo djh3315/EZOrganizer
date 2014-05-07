e2e94fdec76b801380eaa2a0f17a3a5e92fb3e3e
robertwarrengilmore <robertwarrengilmore@vm-0.robertwarrengilmore.koding.kd.io>
2014-05-07 18:12:40 +0000

    JS works with one course and one student.

--- scripts/ourModel.js
+++ scripts/ourModel.js
@@ -1,4 +1,6 @@
 var AssView;
+var currentCourse;
+var currentUser;
 
 $(function(){
   /*
@@ -43,6 +45,7 @@ $(function(){
         name: "new assignment...",
         order: AllAssignments.nextOrder(),
         dueDate: new Date(),
+        description: null,
         studentComplete: false,
         parentComplete: false,
         course: null
@@ -134,16 +137,10 @@ $(function(){
       "click a.delete" : "destroy",
       "input *"      : "save"
     },
-
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-    // app, we set a direct reference on the model for convenience.
     initialize: function() {
       this.listenTo(this.model, 'change', this.render);
       this.listenTo(this.model, 'destroy', this.remove);
     },
-
-    // Re-render the titles of the todo item.
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       this.$el.toggleClass('studentComplete', this.model.get('studentComplete'));
@@ -166,7 +163,7 @@ $(function(){
         this.model.save({
           name: this.$el.find("#name").val(),
           description: this.$el.find("#description").val(),
-          dueDate: this.$el.find("#dueDate").val();
+          dueDate: this.$el.find("#dueDate").val()
         });
         this.$el.removeClass("unsaved");
         lastSaved = new Date;
@@ -185,7 +182,11 @@ $(function(){
       "click a": "select"
     },
     select: function(){
-      //TODO Display the assignments for the given course.
+      // Change the display of the course view.
+      AllCourses.
+      this.$el.addClass();
+      
+      // Display the assignments for the given course.
       currentCourse = this.model;
       AssView.reset;
     }
@@ -195,85 +196,49 @@ $(function(){
    * App views
    */
   var AssignmentsView = Backbone.View.extend({
-    el: $("ul#assignments"),
+    el: $("main#assignments"),
     events: {
       "click #new-assignment":  "create"
     },
     initialize: function() {
-      this.listenTo(AllAssignments, 'add', this.addOne);
+      this.listenTo(AllAssignments, 'add', this.add);
       this.listenTo(AllAssignments, 'reset', this.reset);
-
-      this.main = $('main>ul#assignments');
 
       AllAssignments.fetch();
     },
     add: function(assignment) {
       if(assignment.course == currentCourse) {
         var view = new AssignmentView({model: assignment});
+        console.log(view.render().el);
       	this.$("#assignmentList").append(view.render().el);
       }
     },
     reset: function() {
       this.$("#assignmentList").empty();
       if(currentCourse != null)
-        currentCourse.assignments.each(this.addOne, this);
+        currentCourse.assignments.each(this.add, this);
     },
     create: function() {
       if(currentCourse != null)
-     	 AllAssignments.create({course: currentCourse});
+        AllAssignments.create({course: currentCourse});
     }
   });
   AssView = new AssignmentsView;
+  /* 
   var UserView = Backbone.View.extend({
-
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
     el: $("#userList"),
-
-    // Our template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
-
-    // Delegated events for creating new items, and clearing completed ones.
-    events: {
-      "keypress #new-todo":  "createOnEnter",
-      "click #clear-completed": "clearCompleted",
-      "click #toggle-all": "toggleAllComplete"
-    },
-
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
+    template: _.template($('#user-template').html()),
     initialize: function() {
-
-      this.input = this.$("#new-todo");
-      this.allCheckbox = this.$("#toggle-all")[0];
-
-      this.listenTo(Todos, 'add', this.addOne);
+      this.listenTo(AllStudents, 'add', this.addOne);
+      this.listenTo(AllParents, 'add', this.addOne);      
+      this.listenTo(AllTeachers, 'add', this.addOne);
       this.listenTo(Todos, 'reset', this.addAll);
       this.listenTo(Todos, 'all', this.render);
 
-      this.footer = this.$('footer');
-      this.main = $('#main');
-
-      Todos.fetch();
-    },
-
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
-    render: function() {
-      var done = Todos.done().length;
-      var remaining = Todos.remaining().length;
-
-      if (Todos.length) {
-        this.main.show();
-        this.footer.show();
-        this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
-      } else {
-        this.main.hide();
-        this.footer.hide();
-      }
-
-      this.allCheckbox.checked = !remaining;
+      AllStudents.fetch();
+      AllParents.fetch();
+      AllTeachers.fetch();
+      
     },
 
     // Add a single todo item to the list by creating a view for it, and
@@ -286,33 +251,10 @@ $(function(){
     // Add all items in the **Todos** collection at once.
     addAll: function() {
       Todos.each(this.addOne, this);
-    },
-
-    // If you hit return in the main input field, create new **Todo** model,
-    // persisting it to *localStorage*.
-    createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-      if (!this.input.val()) return;
-
-      Todos.create({title: this.input.val()});
-      this.input.val('');
-    },
-
-    // Clear all done todo items, destroying their models.
-    clearCompleted: function() {
-      _.invoke(Todos.done(), 'destroy');
-      return false;
-    },
-
-    toggleAllComplete: function () {
-      var done = this.allCheckbox.checked;
-      Todos.each(function (todo) { todo.save({'done': done}); });
     }
-
   });
-
-  // Finally, we kick things off by creating the **App**.
   var UsView = new UserView;
+  */
   
   /*
    * Dummy Users and Courses
@@ -321,18 +263,22 @@ $(function(){
     name: "Ms. Fitz"
   });
   AllTeachers.add(t1);
-  var s1 = new Student{
+  var s1 = new Student({
     name: "Billy Klubbe"
   });
   AllStudents.add(s1);
-  AllParents.create({
+  var p1 = new Parent({
     name: "Mr. Klubbe",
-    children: AllStudents.where({name: "Billy Klubbe"})
+    children: [s1]
   });
-  AllCourses.create({
+  AllParents.add(p1);
+  var c1 = new Course({
     name: "Introduction to Horse Whispering",
-    students: AllStudents.where({name: "Billy Klubbe"}),
-    teacher: 
+    students: [s1],
+    teacher: t1
   });
-      
+  AllCourses.add(c1);
+  
+  currentCourse = c1;
+  currentUser = s1;
 });
